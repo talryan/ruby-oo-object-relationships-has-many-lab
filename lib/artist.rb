9a5e31a2582df2b416adabce22ed76b92a52604f
talryan <nataliamryan@gmail.com>
2020-11-03 20:31:28 -0800

    commit tree da9eedee79275c00142242e920a226f08c11ca55

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,7 @@ class Artist
     end
   
     def songs
-     Song.all.select {|song| song.artist == self}
+      Song.all.select {|song| song.artist == self}
     end
 
     def add_song_by_name(song_name)
